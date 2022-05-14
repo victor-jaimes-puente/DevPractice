0a6396395a83bc60efc5c24061c678423dadd94b
dapper-development <59849638+victor-jaimes-puente@users.noreply.github.com>
2022-05-13 21:07:23 -0500

    added codecampjs

---
+++ CodeCampJS/exercies.js
@@ -0,0 +1,51 @@
+// dapper 
+/* mulitline 
+comment 
+here  
+*/
+
+// declare-string-variables
+
+var myFirstName = "Dapper";
+var myLastName = "Tyrant"; 
+
+// *** UNINITIALIZED VARIABLES 
+
+// Only change code below this line
+var a = 5;
+var b = 10 ;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// *** Understanding Case Sensitivity in Variables
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// *** Explore Differences Between the var and let Keywords
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// *** Declare a Read-Only Variable with the const Keyword
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+// *** 
+
+
+
