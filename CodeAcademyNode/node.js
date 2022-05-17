5926979a797a26dbc478dcdd492a6bd61b30ecc8
dapper-development <59849638+victor-jaimes-puente@users.noreply.github.com>
2022-05-16 20:58:20 -0500

    added console log for current problem

--- CodeAcademyNode/node.js
+++ CodeAcademyNode/node.js
@@ -1,1 +1,23 @@
 // Starting NodeJS from the begining 
+
+/*
+Will need to review some basic JS concepts in order to fully work with node. 
+
+arrow expressions
+non-blocking i/o
+setInterval + setTimeout
+---
+
+/ Defining an anonymous arrow expression that simply logs a string to the console.
+console.log(() => console.log('Shhh, Im anonymous'));
+ 
+// Defining a named function by creating an arrow expression and saving it to a const variable helloWorld. 
+const helloWorld = (name) => {
+  console.log(`Welcome ${name} to Codecademy, this is an arrow expression.`)
+};
+ 
+// Calling the helloWorld() function.
+helloWorld('Codey'); //Output: Welcome Codey to Codecademy, this is an Arrow Function Expression.
+
+--
+*/
