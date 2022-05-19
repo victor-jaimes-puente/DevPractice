3d8b0fdf25930da586f62ac637a3972e2a9a169a
dapper-development <59849638+victor-jaimes-puente@users.noreply.github.com>
2022-05-18 21:43:59 -0500

    working on redux review, finished reducer

--- CodeAcademyNode/node.js
+++ CodeAcademyNode/node.js
@@ -79,10 +79,47 @@ $
 */
 // **** 3
 // **** 4
+// Define reducer here
 
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'songs/addSong': {
+      return [...state, action.payload]
+    } 
+    case 'todos/removeAll' : {
+      return [];
+    } 
+    case 'songs/addSong': {
+      return [...state, action.payload];
+    } 
+    case 'songs/removeSong': {
+      return (state.filter(song => song !== action.payload ))
+    }
+    default: {
+      return state;
+    }
+    }
+  }
+
+const initialState = [ 'Take Five', 'Claire de Lune', 'Respect' ];
+
+const addNewSong = {
+  type: 'songs/addSong',
+  payload: 'Halo'
+};
+
+const removeSong = {
+  type: 'songs/removeSong',
+  payload: 'Take Five'
+};
+
+const removeAll = {
+  type: 'songs/removeAll'
+}
 // **** 4
-// **** 
-// **** 
+// **** 5
+
+// **** 5
 // **** 
 // **** 
 // **** 
