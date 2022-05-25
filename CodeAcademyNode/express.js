3162e4851013759bb6960d2c4f128a9014821a54
dapper-development <59849638+victor-jaimes-puente@users.noreply.github.com>
2022-05-25 16:40:47 -0500

    I am still working on express. Finished another section.

--- CodeAcademyNode/express.js
+++ CodeAcademyNode/express.js
@@ -96,6 +96,49 @@ app.listen(PORT, () => {
 
 // ---
 
+// Let’s make sure that our GET /expressions/:id route handles invalid requests properly, for instance if we request an expression ID that does not exist.
+
+// Complete your route so that it sends back the correct expression object if it exists and sends back a 404 response if it does not.
+
+const express = require("express");
+const app = express();
+
+// Serves Express Yourself website
+app.use(express.static("public"));
+
+const { getElementById, seedElements } = require("./utils");
+
+const expressions = [];
+seedElements(expressions, "expressions");
+
+const PORT = process.env.PORT || 4001;
+// Use static server to serve the Express Yourself Website
+app.use(express.static("public"));
+
+app.get("/expressions", (req, res, next) => {
+  res.send(expressions);
+});
+
+app.get("/expressions/:id", (req, res, next) => {
+  const foundExpression = getElementById(req.params.id, expressions);
+  if (foundExpression) {
+    res.status(200).send(
+      foundExpression
+      );
+  } else {
+    res.status(404).send(
+      "Monster Not Found"
+    )
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+
+//
+
 
 
 
