20121120f226beee47dbfc2dadd9905d6bac683c
dapper-development <59849638+victor-jaimes-puente@users.noreply.github.com>
2022-05-25 21:58:54 -0500

    finished another express section

--- CodeAcademyNode/express.js
+++ CodeAcademyNode/express.js
@@ -138,9 +138,195 @@ app.listen(PORT, () => {
 
 
 //
+// For now, open a PUT /expressions/:id route with an empty (req, res, next) callback function. We will fully implement its functionality in the next exercise.
 
 
+const express = require('express');
+const app = express();
 
+// Serves Express Yourself website
+app.use(express.static('public'));
+
+const { getElementById, seedElements } = require('./utils');
+
+const expressions = [];
+seedElements(expressions, 'expressions');
+
+const PORT = process.env.PORT || 4001;
+// Use static server to serve the Express Yourself Website
+app.use(express.static('public'));
+
+app.get('/expressions', (req, res, next) => {
+  res.send(expressions);
+});
+
+app.get('/expressions/:id', (req, res, next) => {
+  const foundExpression = getElementById(req.params.id, expressions);
+  if (foundExpression) {
+    res.send(foundExpression);
+  } else {
+    res.status(404).send();
+  }
+});
+app.put('/expressions/:id', (req, res, next) => {
+  
+});
+
+// Add your PUT route handler below:
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+
+//
+// Use req.query to update the proper element in the expressions array.
+
+// We’ve imported a helper function from /utils.js to help with this task.
+
+// You can use the updateElement() helper function in your PUT /expressions/:id route.
+
+// It takes three arguments:
+
+//     id (the ID number of the element)
+//     queryArguments (the new, updated expression object from req.query)
+//     elementList (the array which contains the element to update)
+
+// updateElement() updates that specific element in the elementList array (you’ll pass in the expressions array), and then returns the updated element.
+
+// Be sure to check that an expression with the id you provide exists in the expressions array (getIndexById() can help)!
+
+// To test your functionality with the Express Yourself machine, make sure your server is run
+
+// ning, get all expressions, and then use the UPDATE tab to select an individual expression, select updates, and send the PUT request.
+// Checkpoint 2 Passed
+
+const express = require('express');
+const app = express();
+
+// Serves Express Yourself website
+app.use(express.static('public'));
+
+const { getElementById, getIndexById, updateElement,
+  seedElements } = require('./utils');
+
+const expressions = [];
+seedElements(expressions, 'expressions');
+
+const PORT = process.env.PORT || 4001;
+// Use static server to serve the Express Yourself Website
+app.use(express.static('public'));
+
+app.get('/expressions', (req, res, next) => {
+  res.send(expressions);
+});
+
+app.get('/expressions/:id', (req, res, next) => {
+  const foundExpression = getElementById(req.params.id, expressions);
+  if (foundExpression) {
+    res.send(foundExpression);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.put('/expressions/:id', (req, res, next) => {
+  const expressionIndex = getIndexById(req.params.id, expressions);
+  if (expressionIndex !== -1) {
+    updateElement(req.params.id, req.query, expressions);
+    res.send(expressions[expressionIndex]);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+//
+
+// Creating An Expression
+
+// POST is the HTTP method verb used for creating new resources. Because POST routes create new data, their paths do not end with a route parameter, but instead end with the type of resource to be created.
+
+// For example, to create a new monster, a client would make a POST request to /monsters. The client does not know the id of the monster until it is created and sent back by the server, therefore POST /monsters/:id doesn’t make sense because a client couldn’t know the unique id of a monster before it exists.
+
+// Express uses .post() as its method for POST requests. POST requests can use many ways of sending data to create new resources, including query strings.
+
+// The HTTP status code for a newly-created resource is 201 Created.
+// Instructions
+// 1.
+
+// Create a POST /expressions route. It should send create and add a new expression to the expressions array if it is a valid new expression (meaning it has an emoji and name key). It should send back the new element with a 201 status code if it is valid, and it should send a 400 status code if the object is not valid.
+
+// You can use the createElement(elementType, objectToCreate) helper function to create a valid expression. The first argument is the type of element, so it should be 'expressions' in this case. The second argument should be the query object with an emoji and a name property. This function will return false if the objectToCreate does not contain all necessary key-value pairs, and it will return the newly-created element if object to create is valid. It does not add the created element to any arrays, you will need to do so yourself.
+
+// Don’t forget to restart your server and test as you implement the functionality. To test your route, use the POST tab in the upper left corner. Select a name and emoji and send the request to see if your route works as intended.
+// Checkpoint 2 Passed
+
+// You can use the .push() method of the expressions array to add a new element after it has been created, for example:
+
+// const newElement = createElement('emoji', {name: 'example', emoji: ':)'});
+// if (newElement) {
+//   elements.push(newElement);
+// }
+
+const express = require('express');
+const app = express();
+
+// Serves Express Yourself website
+app.use(express.static('public'));
+
+const { getElementById, getIndexById, updateElement,
+        seedElements, createElement } = require('./utils');
+
+const expressions = [];
+seedElements(expressions, 'expressions');
+
+const PORT = process.env.PORT || 4001;
+// Use static server to serve the Express Yourself Website
+app.use(express.static('public'));
+
+app.get('/expressions', (req, res, next) => {
+  res.send(expressions);
+});
+
+app.get('/expressions/:id', (req, res, next) => {
+  const foundExpression = getElementById(req.params.id, expressions);
+  if (foundExpression) {
+    res.send(foundExpression);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.put('/expressions/:id', (req, res, next) => {
+  const expressionIndex = getIndexById(req.params.id, expressions);
+  if (expressionIndex !== -1) {
+    updateElement(req.params.id, req.query, expressions);
+    res.send(expressions[expressionIndex]);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.post('/expressions', (req, res, next) => {
+  const receivedExpression = createElement('expressions', req.query);
+  if (receivedExpression) {
+    expressions.push(receivedExpression);
+    res.status(201).send(receivedExpression);
+  } else {
+    res.status(400).send();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+// 
 
 
 
