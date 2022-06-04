b0fc5c303957c1e49ed2bf9646f43f3353f096a5
dapper-development <59849638+victor-jaimes-puente@users.noreply.github.com>
2022-06-03 23:55:22 -0500

    adding db to index.js

--- dapper-express/server/index.js
+++ dapper-express/server/index.js
@@ -4,10 +4,14 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 const apiPort = 3000
+const db = require('./db')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
