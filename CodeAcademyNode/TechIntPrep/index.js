a7fdc9f8493a8b031ce74bc0d062dfab44368d0b
victor-jaimes-puente <59849638+victor-jaimes-puente@users.noreply.github.com>
2022-06-10 11:26:51 -0500

    interview prep

---
+++ CodeAcademyNode/TechIntPrep/index.js
@@ -0,0 +1,12 @@
+class Node {
+    constructor(data){
+      this.data = data
+    }
+  }
+  const firstNode= new Node('first')
+  console.log(firstNode.data)
+  module.exports = Node;
+  
+
+//   
+
