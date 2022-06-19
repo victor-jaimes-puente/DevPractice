d32ee65cf3f6116b68ae51fa626b7d00a0fed90e
victor-jaimes-puente <59849638+victor-jaimes-puente@users.noreply.github.com>
2022-06-19 13:01:22 -0500

    finished 64

--- 101/101-exercises.js
+++ 101/101-exercises.js
@@ -1194,7 +1194,7 @@ addToDone("Exercise 63 is correct.")
 // Exercise 64
 // Write a function definition named productOfAll that takes in sequence of numbers and returns the product of multiplying all the numbers together
 let productOfAll = (s) => {
-    
+  return   [...s].reduce((a, b) => a * b);
 }
 assert(productOfAll([1, 2, 3]), 6, "Exercise 64");
 assert(productOfAll([3, 4, 5]), 60, "Exercise 64");
