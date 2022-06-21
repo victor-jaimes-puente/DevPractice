4a76076b6dcfb4ac3e0f909a42a1f382c45c24a2
victor-jaimes-puente <59849638+victor-jaimes-puente@users.noreply.github.com>
2022-06-20 21:22:29 -0500

    finished 65

--- 101/101-exercises.js
+++ 101/101-exercises.js
@@ -1206,7 +1206,9 @@ addToDone("Exercise 64 is correct.")
 
 // Exercise 65
 // Write a function definition named getHighestNumber that takes in sequence of numbers and returns the largest number.
-
+let getHighestNumber = (s) => {
+    return Math.max(...s)
+}
 
 assert(getHighestNumber([1, 2, 3]), 3, "Exercise 65");
 assert(getHighestNumber([1, 5, 2, 3, 4]), 5, "Exercise 65");
