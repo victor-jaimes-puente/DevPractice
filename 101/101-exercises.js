0bdf87ec3a5f56bc998b051be9a095fae204bf4b
victor-jaimes-puente <59849638+victor-jaimes-puente@users.noreply.github.com>
2022-06-25 20:38:36 -0500

    working on 68

--- 101/101-exercises.js
+++ 101/101-exercises.js
@@ -1253,7 +1253,9 @@ addToDone("Exercise 67 is correct.")
 
 // Exercise 68
 // Write a function definition named onlyEvenNumbers that takes in sequence of numbers and returns the even numbers in an array.
-
+let onlyEvenNumbers = (s) => {
+    
+}
 assert(onlyEvenNumbers([1, 2, 3]), [2], "Exercise 68");
 assert(onlyEvenNumbers([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]), [-4, -2, 2, 4], "Exercise 68");
 assert(onlyEvenNumbers([-4, -3, 1]), [-4], "Exercise 68");
