b1c9e3f07316ba69e951442d6d8adbd4ee80dbdf
victor-jaimes-puente <59849638+victor-jaimes-puente@users.noreply.github.com>
2022-07-05 13:57:59 -0500

    working on codecademy

--- SimplyNode.js
+++ SimplyNode.js
@@ -35,3 +35,125 @@ console.log(
 // Use a string method to log the following string without whitespace at the beginning and end of it.
 console.log('    Remove whitespace   ');
 console.log('    Remove whitespace   '.trim())
+/*
+Inside of a console.log(), create a random number with Math.random(), then multiply it by 100.
+
+
+Stuck? Get a hint
+2.
+Now, use Math.floor() to make the output a whole number.
+
+Inside the console.log() you wrote in the last step, put the existing Math.random() * 100 code inside the parentheses of Math.floor().
+
+
+Stuck? Get a hint
+3.
+Find a method on the JavaScript Math object that console.log();s the smallest integer greater than or equal to a decimal number.
+
+Use this method with the number 43.8. Log the answer to the console.
+
+
+Stuck? Get a hint
+4.
+Use the JavaScript documentation to find a method on the built-in Number object that checks if a number is an integer.
+
+Put the number 2017 in the parentheses of the method and use console.log() to print the result.
+*/
+ 
+console.log(Math.floor(Math.random() * 50) * 100);
+
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017))
+
+// Higher Order Functions  
+
+console.log("----------------------");
+
+// We’ve defined a function with a very long name: checkThatTwoPlusTwoEqualsFourAMillionTimes(). This function takes a long time to execute. It checks whether 2 + 2 = 4, but it does it a million times (just to be really sure)!
+
+// Using const, declare a shorter-named variable, isTwoPlusTwo that will be easier to work with. Assign checkThatTwoPlusTwoEqualsFour
+
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+    for(let i = 1; i <= 1000000; i++) {
+      if ( (2 + 2) != 4) {
+        console.log('Something has gone very wrong :( ');
+      }
+    }
+  };
+  
+  // Write your code below
+  
+const isTwoPlusTwo = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+
+// isTwoPlusTwo()
+// isTwoPlusTwo();
+console.log(isTwoPlusTwo.name);
+
+console.log("----------------------");
+
+const higherOrderFunc = param => {
+    param();
+    console.log( `I just invoked ${param.name} as a callback function!`);
+  }
+   
+  const anotherFunc = () => {
+    console.log( 'I\'m being invoked by the higher-order function!');
+  }
+   
+  higherOrderFunc(anotherFunc);
+
+//   Here we have a function, addTwo(), that adds 2 to whatever is passed into it. Below that, we’ve created what will be a higher-order function, checkConsistentOutput(). The purpose of the higher-order function will be to check the work of addTwo(). Let’s get started!
+
+// To begin, inside the body of checkConsistentOutput(), declare two variables: checkA and checkB:
+
+// checkA stores the sum val plus 2.
+// checkB stores the invocation of the func callback, with val as the argument.
+
+const addTwo = num => {
+    return num + 2;
+  }
+  
+    const checkConsistentOutput = (func, val) => {
+          let checkA = val + 2; 
+          let checkB = func(val);
+          if (checkA === checkB){
+            return checkB
+          } return "inconsistent results"
+    }
+  
+  console.log(checkConsistentOutput(addTwo, 9));
+  
+  // ITERATORS
+
+
+  const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+
+artists.forEach(artist => {
+  console.log(artist + ' is one of my favorite artists.');
+});
+
+const numbers = [1, 2, 3, 4, 5];
+
+const squareNumbers = numbers.map(number => {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+const onlyNumbers = things.filter(thing => {
+  return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
+
+// Iterate over the fruits array to log I want to eat a plus the name of each fruit to the console. For example, I want to eat a mango.
+
+// You may use any form of callback you prefer.
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+fruits.forEach(fruit => {
+    console.log("I want to eat a " + fruit);
+})
