2a81edd7371ccbc3d3964b59d171667bb2f88314
victor-jaimes-puente <59849638+victor-jaimes-puente@users.noreply.github.com>
2023-04-13 00:46:28 -0500

    update

--- express_1/src/index.ts
+++ express_1/src/index.ts
@@ -3,12 +3,12 @@ import app from './server'
 
 // Import the `dotenv` module to load environment variables
 import * as dotenv from 'dotenv'
-
+import config from './config'
 // Load environment variables from the `.env` file
 dotenv.config()
 
 // Set the port number to listen on
-let port = 3000
+let port = config.port
 
 // Start the server and listen on the specified port
 app.listen(port, ()=>{
